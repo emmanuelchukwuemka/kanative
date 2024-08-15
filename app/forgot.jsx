5514a94bec6f9a72ab9d6c669e69c5f0eb5dd67c
hassan taoheed <pencilng@gmail.com>
2024-08-15 02:24:07 +0100

    continue from oluwaseun branch

---
+++ app/forgot.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import ForgotPassword from '../Components/ForgotPassword/ForgotPassword'
+
+const forgot = () => {
+  return (
+    <View>
+      <ForgotPassword/>
+    </View>
+  )
+}
+
+export default forgot
+
+const styles = StyleSheet.create({})
